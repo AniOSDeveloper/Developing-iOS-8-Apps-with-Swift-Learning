305ea4b5c1e071d19393cce1ae99ec81f0709a84
wdxtub <dacrocodilee@gmail.com>
2015-12-15 12:51:41 -0500

    class 6 milestone

--- Happiness/Happiness/FaceView.swift
+++ Happiness/Happiness/FaceView.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
+
+protocol FaceViewDataSource: class {
+    func smilinessForFaceView(sender: FaceView) -> Double?
+}
+
+@IBDesignable
 class FaceView: UIView {
-
-
+    @IBInspectable
     var lineWidth: CGFloat = 3 { didSet {setNeedsDisplay()} }
-    
+    @IBInspectable
     var color: UIColor = UIColor.blueColor() { didSet {setNeedsDisplay() } }
-    
+    @IBInspectable
     var scale: CGFloat = 0.9 { didSet {setNeedsDisplay()} }
     
     var faceCenter: CGPoint {
@@ -24,6 +29,9 @@ class FaceView: UIView {
     var faceRadius: CGFloat {
         return min(bounds.size.width, bounds.size.height) / 2 * scale
     }
+    
+    
+    weak var dataSource: FaceViewDataSource?
     
     private struct Scaling {
         static let FaceRadiusToEyeRadiusRatio: CGFloat = 10
@@ -72,6 +80,14 @@ class FaceView: UIView {
         return path
     }
     
+    
+    func scale(gesture: UIPinchGestureRecognizer){
+        if gesture.state == .Changed {
+            scale *= gesture.scale
+            gesture.scale = 1
+        }
+    }
+    
     override func drawRect(rect: CGRect) {
         // Drawing code
         let facePath = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
@@ -82,7 +98,7 @@ class FaceView: UIView {
         bezierPathForEye(.Left).stroke()
         bezierPathForEye(.Right).stroke()
         
-        let smiliness = -0.5
+        let smiliness = dataSource?.smilinessForFaceView(self) ?? -0.0
         let smilePath = bezierPathForSmile(smiliness)
         smilePath.stroke()
     }
--- Happiness/Happiness/ViewController.swift
+++ Happiness/Happiness/ViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, FaceViewDataSource{
+    
+    
+    @IBOutlet weak var faceView: FaceView! {
+        didSet{
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private struct Constants{
+        static let HappinessGestureScale: CGFloat = 4
     }
-
-
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state{
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
+        }
+        
+    }
+    
+    
+    var happiness: Int = 100 { // 0 = very sad, 100 = ecstatic
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            print("happiness = \(happiness)")
+            updateUI()
+        }
+    }
+    
+    private func updateUI(){
+        faceView.setNeedsDisplay()
+    }
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness - 50) / 50
+    }
 }
 
